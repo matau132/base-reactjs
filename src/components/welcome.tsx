b4ed2964fdccb67119216753e45bb4d6caade829
matau132 <matau135@gmail.com>
2022-01-04 15:45:20 +0700

    first commit

---
+++ src/components/welcome.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { IndexedObject } from '../utils/type';
+
+const WelcomePage: React.FC<IndexedObject> = () => {
+  return (
+    <div className="Article">
+      <div>
+        <h1>Welcome Page</h1>
+      </div>
+    </div>
+  );
+};
+
+export default WelcomePage;
